930bc355da920f4612678ab99e95bae6fcca0312
Firito <yuzhefirito@gmail.com>
2021-08-27 21:22:52 +0800

    添加项目文件。

---
+++ Demoland.Wpf.Calendar/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Demoland.Wpf.Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Calendar_OnSelectedDatesChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            YearTextBlock.Text = Calendar.SelectedDate?.ToString("yyyy年");
+            DateTextBlock.Text = Calendar.SelectedDate?.ToString("M月dd日");
+            WeekTextBlock.Text = Calendar.SelectedDate?.ToString("dddd");
+        }
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
